b0821060515bfefd4601aad0817444b85049878a
Dmitry <diman2014diman@gmail.com>
2021-11-01 09:08:29 +0300

    Initial Commit

---
+++ LogApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogApp
+//
+//  Created by Дмитрий  Ванчугов on 01.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
